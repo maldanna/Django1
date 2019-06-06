a9619210eace7a3186f9b4e9a17949e716561bb6
maldanna <40051663+maldanna@users.noreply.github.com>
2019-06-06 14:49:00 +0530

    Update interview-questions.py

--- interview-questions.py
+++ interview-questions.py
@@ -2,7 +2,9 @@ Both are totally different where the redirect gives the HttpResponseRedirect for
 
 example
 
-return redirect('https://example.com/')  
+return redirect('https://example.com/')   in this we cant send context just will forward url to template or
+outside websites i..e httpresponse
+
 takes you to the https://example.com/ page
  where as render will take you to template 
 
