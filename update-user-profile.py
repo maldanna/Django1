58e698c109621f7982c10683a1a042dc923dd361
maldanna <40051663+maldanna@users.noreply.github.com>
2019-06-06 16:32:22 +0530

    Create update-user-profile.py

---
+++ update-user-profile.py
@@ -0,0 +1,13 @@
+in this we are going to extend User(built in user model) model
+
+form django.db import model
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
