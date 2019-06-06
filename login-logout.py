b5b01c94c94d5d548fe789bd8175352ae523e455
maldanna <40051663+maldanna@users.noreply.github.com>
2019-06-06 16:08:56 +0530

    Update login-logout.py

--- login-logout.py
+++ login-logout.py
@@ -1,4 +1,13 @@
+note:we can goto particular template from projects urls as well as from app urls based on type code 
+  to forward to url to template we have to import app's views in project urls page 
+
+
 form django.contri.auth as auth_views
 
 geenerally django providing super authentication in built system for all authentication purpose
+
+1. login and logout are in built class based views  we have to send url form projects urls only 
+2.login and logout and remaing stuff(rest confirmatin ..etc)  for all this we have to implement templates
+
+
 
