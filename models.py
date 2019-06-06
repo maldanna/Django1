8c9bcf4760ca77f3ad2de7a57274b9e3496b190c
maldanna <40051663+maldanna@users.noreply.github.com>
2019-06-06 12:34:37 +0530

    Update models.py

--- models.py
+++ models.py
@@ -12,7 +12,16 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # we can setnull on_delete=models.SET_NULL
+    
+    #in oracle
+    CONSTRAINT fk_supplier
+    FOREIGN KEY (supplier_id)
+    REFERENCES supplier(supplier_id)
+    ON DELETE SET NULL  # on delete cascade
 
     def __str__(self):
-        return self.title
+        return self.title  # it shows for objects like modelname.objects.all()
+    #if you not mention then reference of object will be display
+    
+    
