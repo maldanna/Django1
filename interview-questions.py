b8a5fbe1f581fd6f85a0a5fd84a87135265aa1da
maldanna <40051663+maldanna@users.noreply.github.com>
2019-06-06 14:25:59 +0530

    Create interview-questions.py

---
+++ interview-questions.py
@@ -0,0 +1,7 @@
+Both are totally different where the redirect gives the HttpResponseRedirect for the argument you have passed.
+
+example
+
+return redirect('https://example.com/')  
+takes you to the https://example.com/ page
+
